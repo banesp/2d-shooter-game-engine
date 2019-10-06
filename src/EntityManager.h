dc8efd2bdee5eaf2dfaef9569e02c74284d69ea1
Espen Bang <bang@noisolation.com>
2019-10-06 13:42:25 +0200

    Section 7: Maps and tiles

---
+++ src/EntityManager.h
@@ -0,0 +1,22 @@
+#ifndef ENTITYMANAGER_H
+#define ENTITYMANAGER_H
+
+#include "./Entity.h"
+#include "./Component.h"
+#include <vector>
+
+class EntityManager {
+    private:
+        std::vector<Entity*> entities;        
+    public:
+        void ClearData();
+        void Update(float deltaTime);
+        void Render();
+        bool HasNoEntities() const;
+        unsigned int GetEntityCount() const;
+        void ListAllEntities() const;
+        std::vector<Entity*> GetEntities() const;
+        Entity& AddEntity(std::string entityName);
+};
+
+#endif
---
+++ src/Game.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include "./Constants.h"
+#include "./Game.h"
+#include "./AssetManager.h"
+#include "./Components/TransformComponent.h"
+#include "./Components/SpriteComponent.h"
+#include "./Components/KeyboardControlComponent.h"
+
+EntityManager manager;
+AssetManager* Game::assetManager = new AssetManager(&manager);
+SDL_Renderer* Game::renderer;
+SDL_Event Game::event;
+
+Game::Game() {
+    this->isRunning = false;
+}
+
+Game::~Game() {
+}
+
+bool Game::IsRunning() const {
+    return this->isRunning;
+}
+
+void Game::Initialize(int width, int height) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        width,
+        height,
+        SDL_WINDOW_BORDERLESS
+    );
+    if (!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+        return;
+    }
+
+    LoadLevel(0);
+
+    isRunning = true;
+    return;
+}
+
+void Game::LoadLevel(int levelNumber) {
+    /* Start including new assets to the assetmanager list */
+    assetManager->AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
+    assetManager->AddTexture("chopper-image", std::string("./assets/images/chopper-spritesheet.png").c_str());
+
+    Entity& chopperEntity(manager.AddEntity("tank"));
+    chopperEntity.AddComponent<TransformComponent>(240, 106, 0, 0, 32,  32, 1);
+    chopperEntity.AddComponent<SpriteComponent>("chopper-image", 2, 90, true, false);
+    chopperEntity.AddComponent<KeyboardControlComponent>("up", "right", "down", "left", "space");
+
+    /* Start including entities and also components to them */
+    Entity& tankEntity(manager.AddEntity("tank"));
+    tankEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
+    tankEntity.AddComponent<SpriteComponent>("tank-image");
+}
+
+void Game::ProcessInput() {
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT: {
+            isRunning = false;
+            break;
+        }
+        case SDL_KEYDOWN: {
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                isRunning = false;
+            }
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+void Game::Update() {
+    // Wait until 16ms has ellapsed since the last frame
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TARGET_TIME));
+
+    // Delta time is the difference in ticks from last frame converted to seconds
+    float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
+
+    // Clamp deltaTime to a maximum value
+    deltaTime = (deltaTime > 0.05f) ? 0.05f : deltaTime;
+
+    // Sets the new ticks for the current frame to be used in the next pass
+    ticksLastFrame = SDL_GetTicks();
+
+    manager.Update(deltaTime);
+}
+
+void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    SDL_RenderClear(renderer);
+
+    if (manager.HasNoEntities()) {
+        return;
+    }
+
+    manager.Render();
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Destroy() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
